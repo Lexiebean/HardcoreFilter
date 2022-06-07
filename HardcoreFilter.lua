a9d29766c24fe4c02ace171747af6e3912db45ad
Lexie <6937574+Lexiebean@users.noreply.github.com>
2022-06-08 09:37:58 +1000

    Add files via upload

--- HardcoreFilter.lua
+++ HardcoreFilter.lua
@@ -1,14 +1,16 @@
 HardcoreFilter_ChatFrame_OnEvent = ChatFrame_OnEvent
-local HardcoreFilter_Level = 20
+local HardcoreFilter_Level = 20 -- Filters messages about players that are a lower level than this value. Set to 61 to filter all messages.
+-- Please note that ~70% of Hardcore players that make it to level 10 don't make it past level 20. And ~90% don't make it past level 30.
+-- So the default value of 20 will filter out ~70% of messages. 
 
 function ChatFrame_OnEvent(event)
 
 	if (event == "CHAT_MSG_SYSTEM") then
 		_, _, Dchr, Dlvl = string.find(arg1,"A tragedy has occurred. Hardcore character (%a+) has fallen in combat at level (%d+)")
 		_, _, Lchr, Llvl = string.find(arg1,"(%a+) has reached level (%d+)")
-		if (Dlvl >= HardcoreFilter_Level) then
+		if (Dlvl > HardcoreFilter_Level) then
 			return
-		elseif (Llvl >= HardcoreFilter_Level) then
+		elseif (Llvl > HardcoreFilter_Level) then
 			return
 		end
 	end
